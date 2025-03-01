7a3400b886c1248e6cdd2edd4a6520a048452ab6
prestonpark26 <153689632+prestonpark26@users.noreply.github.com>
2025-02-28 21:25:58 -0700

    some stuff

--- Mission08/Models/TaskContext.cs
+++ Mission08/Models/TaskContext.cs
@@ -13,5 +13,16 @@ public class TaskContext : DbContext
     public DbSet<MyTask> MyTasks { get; set; }
     
     public DbSet<Category> Categories { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Category>().HasData(
+            new Category { CategoryId = 1, CategoryName = "Home"},
+            new Category { CategoryId = 2, CategoryName = "School"},
+            new Category { CategoryId = 3, CategoryName = "Work"},
+            new Category { CategoryId = 4, CategoryName = "Church"}
+        );
+    }
+    
     
 }
